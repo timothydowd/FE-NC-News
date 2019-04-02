3a968d9a8315099c019a3c271ebca68f05a9f57e
timothydowd <timdowd1984@gmail.com>
2019-04-02 23:24:08 +0100

    Add comment post request (body, not username) functionality added to articles/:article_id/comments.

---
+++ front-end-nc-news/src/components/AddCommentForm.jsx
@@ -0,0 +1,62 @@
+import React, { Component } from "react";
+import Axios from 'axios';
+
+class AddCommentForm extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      newComment: {
+        username: "",
+        body: ""
+      }
+    };
+    
+    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleChange = this.handleChange.bind(this)
+   
+  }
+
+  handleChange(event) {
+    
+    this.setState({newComment:{body: event.target.value}})
+    
+
+
+  }
+
+  handleFormSubmit(e) {
+    e.preventDefault();
+
+    //let commentData = this.state.newComment;
+    console.log(this.props.article_id)
+    Axios.post(
+        `https://ncnewstimdowd.herokuapp.com/api/articles/${this.props.article_id}/comments`,
+        {
+            body: this.state.newComment.body,
+            username: "butter_bridge"
+        }
+      )
+  }
+
+  
+
+
+  render() {
+    return (
+      <form className="commentContainer" onSubmit={this.handleFormSubmit}>
+        <div className="form-group">
+            
+            <textarea onChange={this.handleChange} placeholder='Add your thoughts....' rows='10' cols='45'/>
+            <button action={this.handleFormSubmit}>Submit</button>
+            </div>
+        
+    
+      </form>
+    );
+  }
+}
+
+
+
+export default AddCommentForm;
--- front-end-nc-news/src/components/SingleArticle.jsx
+++ front-end-nc-news/src/components/SingleArticle.jsx
@@ -2,90 +2,74 @@ import React, { Component } from 'react';
 import '../App.css';
 import { Link } from '@reach/router'
 import Axios from 'axios';
+import AddCommentForm from '../components/AddCommentForm'
 
 
 //https://ncnewstimdowd.herokuapp.com/api
 
 class SingleArticle extends Component {
 
-  state = { 
-      articleByArticleId: {},
-      commentsByArticleId: []
+    state = { 
+        articleByArticleId: {},
+        commentsByArticleId: []
 
- }
-  
-  render() {
-    const { title, body, author, comment_count, created_at, topic, votes } = this.state.articleByArticleId
-      return(
-          <div>
-              
-              
-                <h1>single article</h1>
-                    <div className='articleContainer'>
-                        <p> Title: {title} </p>
-                        <p> Author: {author} </p>
-                        <p> Body: {body} </p>
-                        <p> Comments: {comment_count} </p>
-                        <p> Created: {created_at} </p>
-                        <p> Topic: {topic} </p>
-                        <p> Likes: {votes} </p>
-                    </div>
-                    <div className='commentsContainer'>
-                        <h3>comments</h3>
-                        <div>
-                            {
-                                this.state.commentsByArticleId.map(comment => {
-                                    return (
-                                            <div key={comment.comment_id} className='commentsContainer' >
-                                                <p> User: {comment.author} </p>
-                                                <p> Comment: {comment.body} </p>
-                                                <p> Created: {comment.created_at} </p>
-                                                <p> Likes: {comment.votes} </p>
-                                            </div>
-                                    )
-                                })   
-                            }
-                        </div>
-                    </div>
-            
-
-          </div>
-      )
-  }
+    }
 
     componentDidMount() {
 
         Promise.all([this.getArticleById(), this.getCommentsByArticleId()])
-        .then(([articleData, commentsData]) =>{
-            console.log('article: ', articleData, 'comments: ', commentsData)
-            this.setState({ 
-                articleByArticleId: articleData, 
-                commentsByArticleId: commentsData
-            })
+            .then(([articleData, commentsData]) =>{
+                this.setState({ 
+                    articleByArticleId: articleData, 
+                    commentsByArticleId: commentsData
+                })
             
-    })
-   }
+            })
+    }
 
+  
+    render() {
+        const { title, body, author, comment_count, created_at, topic, votes } = this.state.articleByArticleId
+        return(
+            <div>
+                
+                
+                    <h1>single article</h1>
+                        <div className='articleContainer'>
+                            <p> Title: {title} </p>
+                            <p> Author: {author} </p>
+                            <p> Body: {body} </p>
+                            <p> Comments: {comment_count} </p>
+                            <p> Created: {created_at} </p>
+                            <p> Topic: {topic} </p>
+                            <p> Likes: {votes} </p>
+                        </div>
+                        <div className='commentsContainer'>
+                            <h3>comments</h3>
 
+                            <AddCommentForm article_id={this.props.article_id}/>
 
-//   getArticleById = () => {
-//     Axios.get(
-//       `https://ncnewstimdowd.herokuapp.com/api/articles/${this.props.article_id}`
-//     )
-//     .then(articleData => {
-//       this.setState({ singleArticle: articleData.data.article })
-//     })
-//   }
+                            <div>
+                                {
+                                    this.state.commentsByArticleId.map(comment => {
+                                        return (
+                                                <div key={comment.comment_id} className='commentsContainer' >
+                                                    <p> User: {comment.author} </p>
+                                                    <p> Comment: {comment.body} </p>
+                                                    <p> Created: {comment.created_at} </p>
+                                                    <p> Likes: {comment.votes} </p>
+                                                </div>
+                                        )
+                                    })   
+                                }
+                            </div>
+                        </div>
+                
 
-//   getCommentsByArticleId = () => {
-//     Axios.get(
-//         `https://ncnewstimdowd.herokuapp.com/api/articles/${this.props.article_id}/comments`
-//       )
-//       .then(commentsData => {
-//           this.setState({ commentsByArticleId: commentsData.data.commentsByArticleId})
-         
-//       })
-//   }
+            </div>
+        )
+    }
+
 
    getArticleById = () => {
        
@@ -107,21 +91,8 @@ class SingleArticle extends Component {
       })
   }
   
-  
-  
-//   doBoth = () => {
-//       return Promise.all([this.getArticleById(), this.getCommentsByArticleId()])
-//       .then(([articleData, commentsData]) =>{
-//           this.setState({ articleByArticleId: articleData.data.article})
-//           this.setState({ commentsByArticleId: commentsData.data.commentsByArticleId})
-//       })
-//   }
 
-  
-
-    
 }
-
 
 
 export default SingleArticle;
